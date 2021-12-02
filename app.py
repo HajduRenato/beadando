f429a27750d60cca666860464951e0aef1ef972b
HajduRenato <91311825+HajduRenato@users.noreply.github.com>
2021-12-03 00:05:26 +0100

    comit

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ if db_exists:
                 password TEXT
             );
         """)
+        cursor.execute(sql, ('root', 'root'))
         print('DB created.')
 else:
     print('DB exists.')
@@ -28,34 +29,71 @@ if udv == "n" or udv == "N":
         password = input("Enter a password:")
         password1 = input("Confirm password:")
         if password == password1:
-            var = cursor.execute(sql, (username, password))
-            udv = "y"
-            asd = cursor.execute("SELECT name, password FROM MY WHERE  name = ? and password = ?", (username, password))
-            connection.commit()
-            row = cursor.fetchone()
-            print('Given datas:\nUsername:  ' + row[0] + ' \nPassword: ', row[1])
-            break
-        print("Passwords do NOT match!")
+            cursor.execute("SELECT name, password FROM MY WHERE  name = ? and password = ?", (username, password))
+            row1 = cursor.fetchone()
+            string = str(row1)
+            cursor.execute("SELECT name FROM MY")
+            row = cursor.fetchall()
+            print(row)
+            if string == username:
+                print("There is already a user with this name!")
+            else:
+
+                udv = "y"
+                if string != '' or string == 'root':
+                    cursor.execute(sql, (username, password))
+                    connection.commit()
+
+                cursor.execute("SELECT name, password FROM MY WHERE  name = ? and password = ?",
+                               (username, password))
+                row = cursor.fetchone()
+                print('Given datas:\nUsername:  ' + row[0] + ' \nPassword: ', row[1])
+                break
+        else:
+            print("Passwords do not match!")
 
 if udv == "y" or udv == "Y":
     while True:
+        print("Please log in.")
         login1 = input("Login:")
         login2 = input("Password:")
         uname = ''
         passwd = ''
-        row = str(cursor.execute("SELECT name, password FROM MY"))
-        row1 = cursor.fetchall()
-
-        print(login1, login2, row1[0])
-        if row[0] == login1 and row[1] == login2:
+        row = str(cursor.execute("SELECT name, password FROM MY WHERE name = ? and password = ?", (login1, login2)))
+        row1 = cursor.fetchone()
+        string1 = str(row1[0])
+        string2 = str(row1[1])
+        if string1 == login1 and string2 == login2:
             print("Welcome")
-        decision = input("What would you like to do?\nA|Write out users\nB|Use calculator\n")
+            break
+        else:
+            print("Wrong username or password!")
+    while True:
+        decision = input("What would you like to do?\nA|Write out users\nB|Use calculator\nC|Exit\n")
         if decision == 'A' or decision == 'a':
             cursor.execute("SELECT name FROM MY")
-            row = str(cursor.fetchall())
+            row = cursor.fetchall()
             print(row)
+        if decision == 'B' or decision == 'b':
+            print("Let me calculate for you!")
+            print("Give me two numbers, and one operator.")
+            num1 = int(input("1. Number:"))
+            num2 = int(input("2. Number:"))
+            sign = input("Operator(+|-|/|*):")
+            v = 0
+            if sign == '+':
+                v = num1 + num2
+            elif sign == '-':
+                v = num1 - num2
+            elif sign == '*':
+                v = num1 * num2
+            elif sign == '/':
+                v = num1 / num2
 
-        break
+            print(str(num1) + ' ' + sign + ' ' + str(num2) + ' = ' + str(v))
+        if decision == 'C' or decision == 'c':
+            print("Good bye!")
+            break
 
 else:
     print("Incorrect username or password.")
