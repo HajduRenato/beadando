a86f79c768bfc2f8aee91bb2ac358fefd86a6047
HajduRenato <91311825+HajduRenato@users.noreply.github.com>
2021-12-02 19:37:23 +0100

    comit

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
-from flask import Blueprint
-from flask import Flask
+import os
+import sqlite3
+import sqlite3 as sl
 
-app = Blueprint('app', __name__)
+db_name = 'MY.db'
+db_exists = not os.path.exists(db_name)
+con = sl.connect('./MY.db')
+connection = sqlite3.connect(db_name)
+cur = con.cursor()
+if db_exists:
 
-app = Flask(__name__)
+    with con:
+        con.execute("""
+            CREATE TABLE MY (
+                name TEXT,
+                password TEXT
+            );
+        """)
+        print('DB created.')
+else:
+    print('DB exists.')
 
-
-def welcome():
-    print("Welcome...")
-    udv = input("Do you have an acount? y/n: ")
-    if udv == "n":
-        while True:
-            username = input("Enter a username:")
-            password = input("Enter a password:")
-            password1 = input("Confirm password:")
-            if password == password1:
-                file = open(username + ".txt", "w")
-                file.write(username + ":" + password)
-                file.close()
-                udv = "y"
-                break
+print("Welcome...")
+udv = input("Do you have an acount? y/n: ")
+if udv == "n":
+    while True:
+        username = input("Enter a username:")
+        password = input("Enter a password:")
+        password1 = input("Confirm password:")
+        if password == password1:
+            var = cur.execute("INSERT INTO MY ( name, password ) VALUES( ?, ?)", (username, password)).rowcount
+            udv = "y"
+            print('Given datas:\nUsername:'+username+'\nPassword: '+password1)
+            break
             print("Passwords do NOT match!")
 
-    if udv == "y":
-        while True:
-            login1 = input("Login:")
-            login2 = input("Password:")
-            file = open(login1 + ".txt", "r")
-            adat = file.readline()
-            file.close()
-            if adat == login1 + ":" + login2:
-                print("Welcome")
-                break
-            print("Incorrect username or password.")
+if udv == "y":
+    while True:
+        login1 = input("Login:")
+        login2 = input("Password:")
+        write = cur.execute('SELECT ?, ? FROM MY', (login1, login2))
+        print("Welcome")
+        break
+
+else:
+    print("Incorrect username or password.")
